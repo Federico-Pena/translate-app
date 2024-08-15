122622eb42a37c792b7f1c9c4a607e39d40a81d1
Federico <federpena22@gmail.com>
2024-08-15 15:09:40 -0300

    fix browser voices

--- frontend/src/components/Controls.jsx
+++ frontend/src/components/Controls.jsx
@@ -1,30 +1,30 @@
-import { useState } from "react";
-import { IconStartRecording, IconStopCircle } from "./icons/Icons";
-import { useContextSettings } from "../hooks/useContextSettings";
-import { useContextVoice } from "../hooks/useContextVoice";
+import { useState } from 'react'
+import { IconStartRecording, IconStopCircle } from './icons/Icons'
+import { useContextSettings } from '../hooks/useContextSettings'
+import { useContextVoice } from '../hooks/useContextVoice'
 
 const Controls = () => {
-  const [isListening, setIsListening] = useState(false);
-  const { voiceGender, dispatch } = useContextSettings();
-  const { startListening, stopListening } = useContextVoice();
+  const [isListening, setIsListening] = useState(false)
+  const { voiceGender, dispatch } = useContextSettings()
+  const { startListening, stopListening } = useContextVoice()
 
   const handleListening = () => {
     if (isListening) {
-      stopListening();
-      setIsListening(false);
+      stopListening()
+      setIsListening(false)
     } else {
-      startListening();
-      setIsListening(true);
+      startListening()
+      setIsListening(true)
     }
-  };
+  }
   return (
     <section className="controls">
       <button
         type="button"
-        className={`round-button ${isListening ? "" : "anim-pLay"}`}
+        className={`round-button ${isListening ? '' : 'anim-pLay'}`}
         onClick={handleListening}
       >
-        <span role="img" aria-label={isListening ? "Stop" : "Start"}>
+        <span role="img" aria-label={isListening ? 'Stop' : 'Start'}>
           {isListening ? <IconStopCircle /> : <IconStartRecording />}
         </span>
       </button>
@@ -35,24 +35,18 @@ const Controls = () => {
           title="Selecione la voz de reproducción"
           value={voiceGender}
           onChange={(e) =>
-            dispatch({ type: "SET_VOICE_GENDER", payload: e.target.value })
+            dispatch({ type: 'SET_VOICE_GENDER', payload: e.target.value })
           }
         >
-          {window.speechSynthesis.getVoices().map(
-            (voice) =>
-              voice.name.includes("Multilingual") && (
-                <option key={voice.name} value={voice.name}>
-                  {voice.name
-                    .split("-")[0]
-                    .replace("Microsoft", "")
-                    .replace("Multilingual Online (Natural)", "")}
-                </option>
-              )
-          )}
+          {window.speechSynthesis.getVoices().map((voice) => (
+            <option key={voice.name} value={voice.name}>
+              {voice.name}
+            </option>
+          ))}
         </select>
       </label>
     </section>
-  );
-};
+  )
+}
 
-export default Controls;
+export default Controls
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,22 +8,16 @@ import languages from '../routes/languages.routes'
 
 const app = express()
 
-// Disable the header 'X-Powered-By'
 app.disable('x-powered-by')
 
-// Parse incoming JSON data.
 app.use(express.json())
 
-// Enable CORS
 app.use(cors(apiConfig.CORS_SETTINGS))
 
-// Log HTTP requests format.
 app.use(logger)
 
-// Serve static files.
 app.use(express.static(path.resolve('./public')))
 
-// Use one router
 app.use(translate)
 app.use(languages)
 app.use('*', express.static(path.resolve('./public')))
