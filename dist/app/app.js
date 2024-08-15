28a3c9f44f388576dc36ee5b71ad7a975490b785
Federico <federpena22@gmail.com>
2024-08-15 14:51:39 -0300

    url prod

--- dist/app/app.js
+++ dist/app/app.js
@@ -11,17 +11,11 @@ const logger_1 = require("../middlewares/logger");
 const translate_routes_1 = __importDefault(require("../routes/translate.routes"));
 const languages_routes_1 = __importDefault(require("../routes/languages.routes"));
 const app = (0, express_1.default)();
-// Disable the header 'X-Powered-By'
 app.disable('x-powered-by');
-// Parse incoming JSON data.
 app.use(express_1.default.json());
-// Enable CORS
 app.use((0, cors_1.default)(apiConfig_1.apiConfig.CORS_SETTINGS));
-// Log HTTP requests format.
 app.use(logger_1.logger);
-// Serve static files.
 app.use(express_1.default.static(node_path_1.default.resolve('./public')));
-// Use one router
 app.use(translate_routes_1.default);
 app.use(languages_routes_1.default);
 app.use('*', express_1.default.static(node_path_1.default.resolve('./public')));
